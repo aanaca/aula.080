2b15b861eb8cd7210571e0436ad4e1286663d441
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-29 20:33:12 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -31,3 +31,4 @@ def index():
     return render_template('index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False), user_all=user_all) 
+
